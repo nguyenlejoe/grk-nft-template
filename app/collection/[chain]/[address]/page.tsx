81b4b2375c6b341fbd23f1099f0d3e8b164e8076
nguyenlejoe <joe_le.1@hotmail.com>
2023-12-13 14:45:14 -0800

    add nft detail

--- app/collection/[chain]/[address]/page.tsx
+++ app/collection/[chain]/[address]/page.tsx
@@ -1,20 +1,10 @@
 'use client'
-import { Button } from "@/components/ui/button";
-import { Flex } from "@radix-ui/themes";
-import Link from "next/link";
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { useContext, useEffect, useState } from "react"
-import { Chain, ChainItem, CovalentClient, NftTokenContract } from "@covalenthq/client-sdk"
-import { COVALENT_API_KEY } from "@/lib/utils"
-import { useToast } from "@/components/ui/use-toast"
+import { Chain } from "@covalenthq/client-sdk"
 import { useRouter } from "next/navigation";
-import { Card } from "@/components/ui/card";
 import { NFTCollectionTokenListView } from "@covalenthq/goldrush-kit";
 
 export default function Collection({ params }: { params: { chain: Chain, address: string } }) {
   const router = useRouter();
-
 
   return (
     <NFTCollectionTokenListView
